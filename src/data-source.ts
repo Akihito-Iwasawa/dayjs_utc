023ea8bb09585db11b894c64d4efd60ffbb52c02
Akihito-Iwasawa <akihito.iwasawa@tam-bourine.co.jp>
2022-09-05 17:36:41 +0900

    first commit

---
+++ src/data-source.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+import * as dotenv from "dotenv"
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
